f3b7d6ceb35e499be7ea6a3ca52cb43d397662e5
Alina <sharunova-alina2014@yandex.ru>
2025-03-16 12:47:17 +0300

    First Commit

--- Tests/test_check_icon_demoqa.py
+++ Tests/test_check_icon_demoqa.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenuim.webdriver.common.by import By
 
-webdriver.Chrome()
-driver.get("https://demoqa.com/")
+def test_check_icon():
+    webdriver.Chrome()
+    driver.get("https://demoqa.com/")
 
-icon = driver.find_element(By.CSS_SELECTOR,'#app > header > a') #переменная icon к которой обращаемся через драйвер с помощью метода find_element(первым эл-том передаём тип поиска,вторым тип локатора, который будем искать)
-if icon is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+    icon = driver.find_element(By.CSS_SELECTOR,'#app > header > a') #переменная icon к которой обращаемся через драйвер с помощью метода find_element(первым эл-том передаём тип поиска,вторым тип локатора, который будем искать)
+    if icon is None:
+        print('Элемент не найден')
+    else:
+        print('Элемент найден')
